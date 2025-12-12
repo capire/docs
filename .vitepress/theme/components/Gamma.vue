5bdc25dfd963aa8b465d1b116ef4b954f27ed86f
Daniel Hutzel <daniel.hutzel@sap.com>
2025-12-12 19:26:57 +0100

    Introduced Gamma badge and made badges clickable

---
+++ .vitepress/theme/components/Gamma.vue
@@ -0,0 +1,8 @@
+<template>
+  <Badge
+    type="tip"
+    text="Gamma"
+    title="Gamma features are finalized and ready to use, stable, and supported long term. Yet, as they have a broad scope and range, you should ensure to test them thoroughly."
+    onclick="location = '../get-started/learning-sources#feature-status-badges'" style="cursor: pointer"
+  />
+</template>
