cd1dcfed058fe28b206f370f1a4c74ffec984d15
Christian Georgi <christian.georgi@sap.com>
2025-12-18 10:50:38 +0100

    Fix badge click, unify implementation

--- .vitepress/theme/components/Alpha.vue
+++ .vitepress/theme/components/Alpha.vue
@@ -1,8 +1,7 @@
 <template>
-  <Badge
+  <StatusBadge
     type="danger"
     text="Alpha"
     title="Alpha features are experimental. They may never be generally available. If released subsequently, the APIs and behavior might change."
-    onclick="location = '../get-started/learning-sources#feature-status-badges'" style="cursor: pointer"
   />
 </template>
