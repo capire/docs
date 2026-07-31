capire/docs

commit c1e4852c60e1437ec9a75d6bb71b639eb858c998

Fix click on StatusBadge in dev mode (#2792)
The onclick navigation for a status badge doesn't seem to carry over the
hash. Fix is to use a normal `<a href=...>` instead. This is also more
straight forward.

.vitepress/theme/components/StatusBadge.vue
@@ -1,17 +1,10 @@
 <template>
-  <Badge
-    :type="type"
-    :text="text"
-    :title="title"
-    @click="click" style="cursor: pointer"
-  />
+  <a :href="withBase('/releases/#status-badges')">
+    <Badge :type="type" :text="text" :title="title" />
+  </a>
 </template>
 
 <script setup lang="ts">
 defineProps<{ type: string, text:string, title:string  }>()
-
 import { withBase } from 'vitepress'
-function click() {
-  location.href = withBase('/releases/index#status-badges')
-}
 </script>

commit 5c0ec66f6e226695a88ccecd87806bf1dea2cb95

Fix Badge text not to appear in outline (#2793)
Got broken by #2792.

.vitepress/theme/components/StatusBadge.vue
@@ -1,5 +1,6 @@
 <template>
-  <a :href="withBase('/releases/#status-badges')">
+  <!-- ignore-header: excludes this link's text from the outline/TOC (see VitePress serializeHeader) -->
+  <a :href="withBase('/releases/#status-badges')" class="ignore-header">
     <Badge :type="type" :text="text" :title="title" />
   </a>
 </template>